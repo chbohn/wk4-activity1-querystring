11601acde38c449a0e6884def226ab1dc5039b35
Kelly McKeown <kpmckeow@uci.edu>
2019-04-23 19:46:30 -0700

    Updated example.

--- src/main/java/wk4/activity1/querystring/Core/StudentRecords.java
+++ src/main/java/wk4/activity1/querystring/Core/StudentRecords.java
@@ -29,26 +29,43 @@ public class StudentRecords {
 
         // Create a new Client
         ServiceLogger.LOGGER.info("Building client...");
+        Client client = ClientBuilder.newClient();
+        client.register(JacksonFeature.class);
 
         // Get the URI for the IDM
         ServiceLogger.LOGGER.info("Building URI...");
+        String IDM_URI = ExampleService.getExampleConfigs().getIdmConfigs().getIdmUri();
 
         ServiceLogger.LOGGER.info("Setting path to endpoint...");
+        String IDM_ENDPOINT_PATH = ExampleService.getExampleConfigs().getIdmConfigs().getPrivilegePath();
 
         // Create a WebTarget to send a request at
         ServiceLogger.LOGGER.info("Building WebTarget...");
+        WebTarget webTarget = client.target(IDM_URI).path(IDM_ENDPOINT_PATH);
 
         // Create an InvocationBuilder to create the HTTP request
         ServiceLogger.LOGGER.info("Starting invocation builder...");
+        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
 
         // Set the payload
         ServiceLogger.LOGGER.info("Setting payload of the request");
+        VerifyPrivilegeRequestModel requestModel = new VerifyPrivilegeRequestModel(email, 3);
 
         // Send the request and save it to a Response
         ServiceLogger.LOGGER.info("Sending request...");
+        Response response = invocationBuilder.post(Entity.entity(requestModel, MediaType.APPLICATION_JSON));
         ServiceLogger.LOGGER.info("Sent!");
 
         // Check that status code of the request
+        if (response.getStatus() == 200) {
+            ServiceLogger.LOGGER.info("Received status 200!");
+            String jsonText = response.readEntity(String.class);
+            ServiceLogger.LOGGER.info("jsonText: " + jsonText);
+            return true;
+        } else {
+            ServiceLogger.LOGGER.warning("Received status " + response.getStatus() + " -- you lose.");
+        }
+
         return false;
     }
 }
